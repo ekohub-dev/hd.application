8d5df2848be74b61337270456e532e04a77c660f
ekodev <jerichomanio32@gmail.com>
2026-03-18 21:11:36 +0800

    Update Require.lua

--- StarterPlayerScripts/Require.lua
+++ StarterPlayerScripts/Require.lua
@@ -1,1 +1,9 @@
+local EZ = require(game.ReplicatedStorage.EZVisualize.EZVisualizer)
 
+task.wait(5)
+
+local model = workspace.VModel
+local sound = workspace.SpawnLocation["Empty Heartbeat"]
+
+EZ.Register(model, sound)
+EZ.Start()
